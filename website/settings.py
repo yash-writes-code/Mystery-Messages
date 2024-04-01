dabe36eea669222a1f8a640c5dc45be105b4b049
Yash Goyal <yashgoyalambk@gmail.com>
2024-04-01 22:26:20 +0530

    Changed settings for deployment

--- website/settings.py
+++ website/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t_2rk99^%_!p=-@qeo9h3yd0*u3x^69qomtwb9@gyy@^539sde'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
