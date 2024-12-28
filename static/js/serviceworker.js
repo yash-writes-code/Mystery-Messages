3a610d41c8a6cb7a62b686fd2f7d634845376088
Yash Goyal <yashgoyalambk@gmail.com>
2024-12-28 17:25:56 +0530

    service worker bug fix

--- static/js/serviceworker.js
+++ static/js/serviceworker.js
@@ -1,26 +1,53 @@
+// var staticCacheName = 'djangopwa-v1';
+
+// self.addEventListener('install', function(event) {
+// event.waitUntil(
+// 	caches.open(staticCacheName).then(function(cache) {
+// 	return cache.addAll([
+// 		'',
+// 	]);
+// 	})
+// );
+// });
+
+// self.addEventListener('fetch', function(event) {
+// var requestUrl = new URL(event.request.url);
+// 	if (requestUrl.origin === location.origin) {
+// 	if ((requestUrl.pathname === '/')) {
+// 		event.respondWith(caches.match(''));
+// 		return;
+// 	}
+// 	}
+// 	event.respondWith(
+// 	caches.match(event.request).then(function(response) {
+// 		return response || fetch(event.request);
+// 	})
+// 	);
+// });
 var staticCacheName = 'djangopwa-v1';
 
 self.addEventListener('install', function(event) {
-event.waitUntil(
-	caches.open(staticCacheName).then(function(cache) {
-	return cache.addAll([
-		'',
-	]);
-	})
-);
+    event.waitUntil(
+        caches.open(staticCacheName).then(function(cache) {
+            return cache.addAll([
+                '/', // Add your main HTML file here
+                // Add other assets you want to cache
+            ]);
+        })
+    );
 });
 
 self.addEventListener('fetch', function(event) {
-var requestUrl = new URL(event.request.url);
-	if (requestUrl.origin === location.origin) {
-	if ((requestUrl.pathname === '/')) {
-		event.respondWith(caches.match(''));
-		return;
-	}
-	}
-	event.respondWith(
-	caches.match(event.request).then(function(response) {
-		return response || fetch(event.request);
-	})
-	);
+    var requestUrl = new URL(event.request.url);
+    if (requestUrl.origin === location.origin) {
+        if (requestUrl.pathname === '/') {
+            event.respondWith(caches.match('/')); // Serve the cached main HTML file
+            return;
+        }
+    }
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
 });
